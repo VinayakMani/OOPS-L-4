154f8f24bcb98da54bc734e8167842640ab29be5
Vinayak Mani (202132) <89988949+VinayakMani@users.noreply.github.com>
2022-03-12 10:59:45 +0530

    Add files via upload

---
+++ oopsL4.cpp
@@ -0,0 +1,50 @@
+//Use of overloading
+//Vinayak Mani, Roll no - 202132
+
+#include<iostream>
+using namespace std;
+class power 
+{ 
+    public: 
+    int result=1; 
+    int m,n; 
+    void display(){ 
+        cout<<"Result is : "<<result; 
+        } 
+        int calsquare(int m,int n)
+        { 
+            for(int i=1;i<=n;i++)
+            { 
+                result=result*m; 
+             } 
+       display(); 
+       return 0; 
+         } 
+                
+    int square(int m, int n)
+         { 
+             calsquare(m,n); 
+             return 0; 
+             }
+             int square(int m)
+                    { 
+                        const int n=2; 
+                        calsquare(m,n); 
+                        return 0; 
+                    } 
+
+             };
+                int main()
+                   { 
+                        power A; 
+                        int m,result; 
+                        cout<<"Enter the value of m : "<<endl; 
+                        cin>> m; 
+                        A.square(m); 
+                        
+                        return 0; 
+
+                    }
+
+
+
